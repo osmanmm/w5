1865bbf3257b0257b7aa43d7af670d733baaa414
osmanmm <mohamed.osman@outlook.com>
2021-08-19 03:11:41 +0000

    first commit

---
+++ net/client.js
@@ -0,0 +1,10 @@
+const connect = function() {
+    const conn = net.createConnection({ 
+      host: 'localhost',
+      port: 50541
+    });
+    // interpret incoming data as text
+    conn.setEncoding('utf8'); 
+  
+    return conn;
+  }
